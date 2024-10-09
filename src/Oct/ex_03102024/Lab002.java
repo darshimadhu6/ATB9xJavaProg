54056cfac8c55ab64951a12492d0bced15a583b0
darshimadhu6 <darshimadhu6@gmail.com>
2024-10-09 18:33:39 +0530

    First push

---
+++ src/Oct/ex_03102024/Lab002.java
@@ -0,0 +1,17 @@
+package Oct.ex_03102024;
+
+public class Lab002 {
+    public static void main (String [] args){
+        System.out.println("Table of 10 -> ");
+        //Single line comments
+//        Ignore the code
+        System.out.println("10*1=10\n10*2=20\n10*3=30\n10*4=40\n10*5=50");
+        System.out.println("10*6=60\n10*7=70\n10*8=80\n10*8=80\n10*10=100");
+
+        /*
+        Multi line comment
+         */
+
+
+    }
+}
---
+++ src/Oct/ex_03102024/Lab003.java
@@ -0,0 +1,15 @@
+package Oct.ex_03102024;
+
+public class Lab003 {
+    public static void main (String [] args)
+    {
+        int c = 4 + 3;
+        int a = 4 -3;
+        int b = 4 * 3;
+        int d = 6 / 3;
+        System.out.println("The sum of 2 numbers is "+c);
+        System.out.println("The diff of 2 numbers is "+a);
+        System.out.println("The product of 2 numbers is "+b);
+        System.out.println("The div of 2 numbers is "+d);
+    }
+}
